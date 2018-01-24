6047105ff02e0bd80e01cbad601fb32dcb1a2e80
sh4hids <shahid.bracu@gmail.com>
2018-01-24 14:56:57 +0600

    Fixed firefox time format issue and modified some styles

--- md-timepicker/md-timepicker.js
+++ md-timepicker/md-timepicker.js
@@ -2,16 +2,32 @@
   'use strict';
   angular.module('myApp').directive('mdTimepicker', mdTimepicker);
 
-  function mdTimepicker() {
+  function mdTimepicker($filter) {
     return {
       restrict: 'E',
       require: 'ngModel',
       replace: true,
-      link: function(scope, element, attrs) {
+      link: function(scope, element, attrs, ngModel) {
         scope.time = moment().second(0).milliseconds(0).toDate();
       },
       templateUrl: './md-timepicker/md-timepicker.html'
     };
   }
+
+  angular.module('myApp').directive('ngModel', function($filter) {
+    return {
+      require: '?ngModel',
+      link: function(scope, elem, attr, ngModel) {
+        if (!ngModel)
+          return;
+        if (attr.type !== 'time')
+          return;
+
+        ngModel.$formatters.unshift(function(value) {
+          return value.replace(/:00\.000$/, '')
+        });
+      }
+    }
+  });
 
 })();
