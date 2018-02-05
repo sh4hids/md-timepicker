c20a3dd203e82ae7afadbff124ca5ab78c36a399
sh4hids <shahid.bracu@gmail.com>
2018-02-05 11:38:03 +0600

    Added default time list if not defined

--- md-timepicker/md-timepicker.js
+++ md-timepicker/md-timepicker.js
@@ -9,6 +9,28 @@
       replace: true,
       link: function(scope, element, attrs, ngModel) {
         scope.time = moment().second(0).milliseconds(0).toDate();
+        if (!scope.timings) {
+          scope.timings = [],
+          scope.generateTimings = function (timeDiff) {
+            console.log("Hello");
+            var periods = ['AM', 'PM'],
+            hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            prop = null,
+            hour = null,
+            min = null;
+
+            for (prop in periods) {
+              for (hour in hours) {
+                for (min = 0; min < 60; min += timeDiff) {
+                  var strTime = ('0' + hours[hour]).slice(-2) + ':' + ('0' + min).slice(-2) + " " + periods[prop];
+                  scope.timings.push(moment(strTime, 'hh:mm a').second(0).milliseconds(0).toDate());
+                }
+              }
+            }
+            return scope.timings;
+          }
+          scope.generateTimings(60);
+        }
       },
       templateUrl: './md-timepicker/md-timepicker.html'
     };
