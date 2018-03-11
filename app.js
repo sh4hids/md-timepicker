ea53be24996c792be35268d65b6770fae07de09f
sh4hids <shahid.bracu@gmail.com>
2018-03-11 09:31:18 +0600

    Updated directive function

--- app.js
+++ app.js
@@ -4,24 +4,5 @@
 
   myApp.controller('myAppController', ['$scope', function ($scope) {
     var vm = this;
-    vm.timings = [];
-    vm.generateTimings = function (timeDiff) {
-      var times = [],
-      periods = ['AM', 'PM'],
-      hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-      prop = null,
-      hour = null,
-      min = null;
-
-      for (prop in periods) {
-        for (hour in hours) {
-          for (min = 0; min < 60; min += timeDiff) {
-            var strTime = ('0' + hours[hour]).slice(-2) + ':' + ('0' + min).slice(-2) + " " + periods[prop];
-            vm.timings.push(moment(strTime, 'hh:mm a').second(0).milliseconds(0).toDate());
-          }
-        }
-      }
-      return vm.timings;
-    }
   }]);
 })();
--- md-timepicker/md-timepicker.js
+++ md-timepicker/md-timepicker.js
@@ -1,57 +1,75 @@
-(function() {
+(function () {
   'use strict';
-  angular.module('myApp').directive('mdTimepicker', mdTimepicker);
+  angular.module('myApp').directive('mdTimepicker', [mdTimepicker]);
 
-  function mdTimepicker($filter) {
-    var attr;
+  function mdTimepicker() {
     return {
       restrict: 'E',
-      require: 'ngModel',
-      replace: true,
-      scope: {customClass: '@customClass'},
-      templateUrl: './md-timepicker/md-timepicker.html',
-      link: function(scope, element, attrs, ngModel) {
-        scope.time = moment().second(0).milliseconds(0).toDate();
-        if (!scope.timings) {
-          scope.timings = [],
-          scope.generateTimings = function (timeDiff) {
-            console.log("Hello");
-            var periods = ['AM', 'PM'],
-            hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-            prop = null,
-            hour = null,
-            min = null;
+      replace: false,
+      scope: {
+        dropdownClass: '@',
+        ngModel: '='
+      },
+      link: function (scope, element, attrs) {
+        if(!scope.ngModel || !isValidTime(scope.ngModel)){
+          scope.ngModel = moment().second(0).milliseconds(0).toDate();
+        } else {
+          scope.ngModel = moment(scope.ngModel).second(0).milliseconds(0).toDate();;
+        }
 
-            for (prop in periods) {
-              for (hour in hours) {
+        function isValidTime(val){
+          return moment(val,"YYYY-MM-DD LT", true).isValid();
+        }
+
+        if (!scope.timings) {
+          scope.timings = [];
+          scope.generateTimings = (function (timeDiff) {
+            var periods = ['AM', 'PM'],
+              hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+              min = null;
+            for (var i = 0; i < periods.length; i++) {
+              for (var j = 0; j < hours.length; j++) {
                 for (min = 0; min < 60; min += timeDiff) {
-                  var strTime = ('0' + hours[hour]).slice(-2) + ':' + ('0' + min).slice(-2) + " " + periods[prop];
-                  scope.timings.push(moment(strTime, 'hh:mm a').second(0).milliseconds(0).toDate());
+                  var strTime = ('0' + hours[j]).slice(-2) + ':' + ('0' +
+                    min).slice(-2) + ' ' + periods[i];
+                  scope.timings.push(moment(strTime, 'hh:mm a').second(
+                    0).milliseconds(0).toDate());
                 }
               }
             }
             return scope.timings;
-          }
-          scope.generateTimings(60);
+          })(60);
         }
-      }
+      },
+      template: '<span class="md-timepicker">' +
+      '<md-input-container class="time-keyboard">' +
+      '<input type="time" name="input" ng-model="ngModel" min="00:00" max="23:59"/>' +
+      '</md-input-container>' +
+      '<md-input-container class="time-dropdown">' +
+      '<md-select ng-model="ngModel" md-container-class="time-list {{dropdownClass}}">' +
+      '<span class="md-timepicker-input-mask">' +
+      '<i class="material-icons">schedule</i> <p>Time ' +
+      '<span>{{ ngModel | date: "hh:mm a" }}</span> </p></span>' +
+      '<md-option ng-value="time" ng-repeat="time in timings">' +
+      '{{ time | date: "hh:mm a"}}</md-option>' +
+      '</md-select>' + '</md-input-container>' +
+      '</span>'
     };
   }
-
-  angular.module('myApp').directive('ngModel', function($filter) {
+  angular.module('myApp').directive('ngModel', function ($filter) {
     return {
       require: '?ngModel',
-      link: function(scope, elem, attr, ngModel) {
-        if (!ngModel)
+      link: function (scope, elem, attr, ngModel) {
+        if (!ngModel) {
           return;
-        if (attr.type !== 'time')
+        }
+        if (attr.type !== 'time') {
           return;
-
-        ngModel.$formatters.unshift(function(value) {
-          return value.replace(/:00\.000$/, '')
+        }
+        ngModel.$formatters.unshift(function (value) {
+          return value.replace(/:00\.000$/, '');
         });
       }
-    }
+    };
   });
-
 })();
